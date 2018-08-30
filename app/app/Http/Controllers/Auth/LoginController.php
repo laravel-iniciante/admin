a4ea8dd35f9a78bd854c886db322fb67b501f060
thiago <thiago122@gmail.com>
2018-08-29 23:24:35 -0300

    alterações no login, criação de controllers e rotas básicas

--- app/app/Http/Controllers/Auth/LoginController.php
+++ app/app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,35 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Seta as chaves e valores necessários para o login
+     * as chaves representam as colunas do banco que deven ser verificadas
+     * e o valor o registro no banco
+     * Neste caso intenção é adicionar a verificação status do usuário 1( ativp )
+     * Só autentica usuários com status ativo
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     * @overide
+     */
+    protected function credentials(\Illuminate\Http\Request $request)
+    {
+        $credentials = $request->only($this->username(), 'password');
+        return array_add($credentials, 'status', '1');
+    }
+
+    /**
+     * Retorna o 'path' para onde deverá ser redirecionado
+     * após ser autenticado com sucesso
+     *
+     *
+     * @override
+     */
+    protected function redirectTo()
+    {
+        // TODO: logica futura aqui para determinar o redirecionamento
+        return '/dashboard';
+    }
+
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -17,4 +17,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group([ 'prefix' => 'dashboard', 'namespace' => 'Dashboard'], function(){
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+    Route::resource('user', 'UserController');
+    Route::resource('product', 'ProductController');
+    Route::resource('permission', 'PermissionController');
+    Route::resource('category', 'CategoryController');
+    Route::resource('role', 'RoleController');
+});
